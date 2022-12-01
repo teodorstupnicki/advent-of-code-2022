21ba0e47ba91d079ff4a13127664f7a23688b61c
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-12-01 14:50:02 +0100

    Day one init

---
+++ 1/target/debug/deps/one-3047b71990fe98a5.d
@@ -0,0 +1,5 @@
+c:\source\advent-of-code-2022\1\target\debug\deps\one-3047b71990fe98a5.rmeta: src\main.rs
+
+c:\source\advent-of-code-2022\1\target\debug\deps\one-3047b71990fe98a5.d: src\main.rs
+
+src\main.rs:
---
+++ 1/target/debug/deps/one-87bf928bf78b7969.d
@@ -0,0 +1,5 @@
+c:\source\advent-of-code-2022\1\target\debug\deps\one-87bf928bf78b7969.rmeta: src\main.rs
+
+c:\source\advent-of-code-2022\1\target\debug\deps\one-87bf928bf78b7969.d: src\main.rs
+
+src\main.rs:
