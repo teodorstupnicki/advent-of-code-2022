34243b54826ddb9fbff0b44c8cb3f22d7d35f583
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-12-01 18:30:05 +0100

    Part 2 of day 1

--- solutions/1/src/main.rs
+++ solutions/1/src/main.rs
@@ -2,21 +2,19 @@ use std::fs;
 
 fn main() {
     let content = fs::read_to_string("data.txt").unwrap();
-    let (mut max0 , mut max1, mut max2) = (0, 0, 0);
     let mut sum = 0;
+    let mut v = Vec::new();
     for line in content.lines() {
         sum = if line.is_empty() {
-            if sum > max0 {
-                max0 = sum;
-            } else if sum > max1 {
-                max1 = sum;
-            } else if sum > max2 {
-                max2 = sum;
-            }
+            v.push(sum);
             0
         } else {
             sum + line.parse::<i32>().unwrap()
         };
     }
+    v.sort();
+    let max0 = v.pop().unwrap();
+    let max1 = v.pop().unwrap();
+    let max2 = v.pop().unwrap();
     println!("Sum of calories: {0}", max0 + max1 + max2);
 }
