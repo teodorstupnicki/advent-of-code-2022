d5d635ba61274acef781ed070df6a51ed0142b20
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-12-01 18:06:02 +0100

    File cleanup

--- solutions/1/src/main.rs
+++ solutions/1/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("data.txt").unwrap();
+    let (mut max0 , mut max1, mut max2) = (0, 0, 0);
+    let mut sum = 0;
+    for line in content.lines() {
+        sum = if line.is_empty() {
+            if sum > max0 {
+                max0 = sum;
+            } else if sum > max1 {
+                max1 = sum;
+            } else if sum > max2 {
+                max2 = sum;
+            }
+            0
+        } else {
+            sum + line.parse::<i32>().unwrap()
+        };
+    }
+    println!("Sum of calories: {0}", max0 + max1 + max2);
 }
--- solutions/1/target/debug/deps/one-87bf928bf78b7969.d
+++
@@ -1,5 +0,0 @@
-c:\source\advent-of-code-2022\1\target\debug\deps\one-87bf928bf78b7969.rmeta: src\main.rs
-
-c:\source\advent-of-code-2022\1\target\debug\deps\one-87bf928bf78b7969.d: src\main.rs
-
-src\main.rs:
