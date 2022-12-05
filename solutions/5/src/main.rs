8d9d917e02346de60d9e463e83a63988ca691de9
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-12-05 23:23:40 +0100

    Added file processing

--- solutions/5/src/main.rs
+++ solutions/5/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("data.txt").unwrap();
+    let mut n=0;
+    for line in contents.lines() {
+        if line.contains("1") {
+            let it = line.split("   ");
+            n = it.last().unwrap().trim().parse::<i32>().unwrap();
+            break;
+        }
+    }
+    println!("n = {0}", n);
+    
 }
