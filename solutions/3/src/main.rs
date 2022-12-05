ee3b76e491b845921808e4e2d8e090b7833ef975
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-12-05 21:13:33 +0100

    Day 3 part 1

--- solutions/3/src/main.rs
+++ solutions/3/src/main.rs
@@ -1,7 +1,34 @@
-use std::fs;
+use std::{fs, collections::HashMap};
 
 fn main() {
     let content = fs::read_to_string("data.txt").unwrap();
     let mut sum = 0;
-    
+    let mut common: char = 'x';
+    let mut key_map = HashMap::new();
+    for line in content.lines() {
+        let length = line.len();
+        let left = &line[..length/2];
+        let right = &line[length/2..length];
+        for c in left.chars() {
+            key_map.insert(c, 2);
+        }
+        for r in right.chars() {
+            let value = key_map.get(&r);
+            match value {
+                Some(v) => if *v == 2 {
+                    common = r;
+                    break;
+                },
+                None => {
+                    key_map.insert(r, 1);
+                },
+            }
+        }
+        key_map.clear();
+        println!("Left: {0}", left);
+        println!("Right: {0}", right);
+        println!("Char: {0}", common);
+
+        println!("Digit: {0}", common as u32 - 'a' as u32 );
+    }
 }
