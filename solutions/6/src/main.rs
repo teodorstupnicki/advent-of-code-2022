6e2d8f4f4848c4f9d17f3c4fceebd4d2d67e5e8e
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-12-06 11:28:31 +0100

    Day 6 part 1 final

--- solutions/6/src/main.rs
+++ solutions/6/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("data.txt").unwrap();
+    let chars: Vec<char> = content.chars().collect();
+    for i in 3..chars.len() {
+        let mut flag = true;
+        for j in 0..4{
+            for k in 0..4 {
+                if j != k && chars[i-j] == chars[i-k] {
+                    flag = false;
+                }
+
+            }
+        }
+        if flag {
+            println!("{0}", i+1);
+            break;
+        }
+    }
+    
 }
